def5f838f381b2edd9a0b5a582778644c103cd12
Nico <93469940+nizikai@users.noreply.github.com>
2024-05-18 03:41:09 +0700

    Created Login Page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/auth', function () {
+    return view('login');
+});
+
